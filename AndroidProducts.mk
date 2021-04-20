a1a19626ec919a1ffd9194d624648ceaf53341bc
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-20 10:53:26 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_1916.mk
+    $(LOCAL_DIR)/superior_1916.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_1916-user \
-    arrow_1916-userdebug \
-    arrow_1916-eng
+    superior_1916-user \
+    superior_1916-userdebug \
+    superior_1916-eng
